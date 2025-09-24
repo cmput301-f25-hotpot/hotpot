082f10a4b4dc5279976954fae6cd4afc24a12317
annebarassougayathri <145299052+annebarassougayathri@users.noreply.github.com>
2025-09-23 19:37:53 -0600

    color:pink

--- app/src/main/java/com/example/hotpot/Shape.java
+++ app/src/main/java/com/example/hotpot/Shape.java
@@ -1,11 +1,8 @@
 package com.example.hotpot;
 
 public class Shape {
-<<<<<<< HEAD
+
     private int x, y;
     String color = "pink";
-=======
-    private int x,y;
-    String color = "black";
->>>>>>> 6b995ea78a01662c73578045b75817e7d39c41d4
+
 }
